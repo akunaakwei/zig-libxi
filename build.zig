704c59b8025c9dc0638e2f6a15e8d885b1895f3d
Akuna Akwei <akunaakwei@gmail.com>
2026-09-20 09:43:48 +0200

    add pic

--- build.zig
+++ build.zig
@@ -5,12 +5,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const linkage = b.option(std.builtin.LinkMode, "linkage", "Linkage type for the library") orelse .static;
+    const pic = b.option(bool, "pic", "Enable PIC") orelse (if (linkage == .dynamic) true else null);
 
     const xi_dep = b.dependency("xi", .{});
     const x11_dep = b.dependency("x11", .{
         .target = target,
         .optimize = optimize,
         .linkage = linkage,
+        .pic = pic,
     });
     const x11 = x11_dep.artifact("x11");
     const xorgproto_dep = b.dependency("xorgproto", .{
@@ -22,12 +24,14 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .linkage = linkage,
+        .pic = pic,
     });
     const xext = xext_dep.artifact("xext");
     const xfixes_dep = b.dependency("xfixes", .{
         .target = target,
         .optimize = optimize,
         .linkage = linkage,
+        .pic = pic,
     });
     const xfixes = xfixes_dep.artifact("xfixes");
 
@@ -35,7 +39,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .link_libc = true,
-        .pic = if (linkage == .dynamic) true else null,
+        .pic = pic,
     });
     mod.linkLibrary(x11);
     mod.linkLibrary(xorgproto);
